e5a60d7e4678d6faea26a3b9b88e0c2e29def9b0
janh <janh@softwareag.com>
2022-09-23 17:34:12 +0200

    added actionbar

---
+++ bookmarks/add-bookmark.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild, ViewContainerRef } from "@angular/core";
+
+@Component({
+  selector: "[c8y-add-bookmark]",
+  template: `
+    <ng-template #template>
+      <li>
+        <button title="Bookmark device" class="btn btn-link">
+          <i [c8yIcon]="'star'"></i>
+          Bookmark device
+        </button>
+      </li>
+    </ng-template>
+  `,
+})
+export class AddBookmarkComponent {
+  @ViewChild("template", { static: true }) template;
+
+  constructor(private viewContainerRef: ViewContainerRef) {}
+
+  ngOnInit() {
+    this.viewContainerRef.createEmbeddedView(this.template);
+  }
+}
--- bookmarks/bookmarks.module.ts
+++ bookmarks/bookmarks.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { CommonModule } from "@c8y/ngx-components";
 import { BookmarksService } from "./bookmarks.service";
-import { HOOK_NAVIGATOR_NODES } from "@c8y/ngx-components";
+import { AddBookmarkService } from "./add-bookmark.service";
+import { HOOK_ACTION_BAR, HOOK_NAVIGATOR_NODES } from "@c8y/ngx-components";
+import { AddBookmarkComponent } from "./add-bookmark.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [AddBookmarkComponent],
   imports: [CommonModule],
   exports: [],
   providers: [
     BookmarksService,
     { provide: HOOK_NAVIGATOR_NODES, useClass: BookmarksService, multi: true },
+    { provide: HOOK_ACTION_BAR, useClass: AddBookmarkService, multi: true }
   ],
 })
 export class BookmarksModule {}
