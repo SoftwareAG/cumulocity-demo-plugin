90c165ccc2b88521a2e817ff1ff633e4e689eacc
janh <janh@softwareag.com>
2022-09-27 14:56:52 +0200

    step4 corrected

--- bookmarks/add-bookmark.component.ts
+++ bookmarks/add-bookmark.component.ts
@@ -1,11 +1,4 @@
 import { Component, ViewChild, ViewContainerRef } from "@angular/core";
-import { Router } from "@angular/router";
-import { InventoryService } from "@c8y/client";
-import {
-  ContextRouteService,
-  getActivatedRoute,
-  NavigatorService,
-} from "@c8y/ngx-components";
 
 @Component({
   selector: "[c8y-add-bookmark]",
@@ -15,9 +8,8 @@ import {
         <button
           title="Bookmark device"
           class="btn btn-link"
-          (click)="bookmark()"
         >
-          <i [c8yIcon]="isBookmarked ? 'star' : 'star-outline'"></i>
+          <i [c8yIcon]="'star'"></i>
           Bookmark device
         </button>
       </li>
@@ -25,35 +17,11 @@ import {
   `,
 })
 export class AddBookmarkComponent {
-  isBookmarked = true;
   @ViewChild("template", { static: true }) template;
 
-  constructor(
-    private viewContainerRef: ViewContainerRef,
-    private router: Router,
-    private inventory: InventoryService,
-    private contextRoute: ContextRouteService,
-    private navigator: NavigatorService
-  ) {}
+  constructor(private viewContainerRef: ViewContainerRef) {}
 
   ngOnInit() {
     this.viewContainerRef.createEmbeddedView(this.template);
-    this.isBookmarked = !!this.contextRoute.getContextData(
-      getActivatedRoute(this.router)
-    )?.contextData?.c8y_IsBookmarked;
-  }
-
-  async bookmark() {
-    const currentDevice = this.contextRoute.getContextData(
-      getActivatedRoute(this.router)
-    )?.contextData;
-    if (currentDevice) {
-      await this.inventory.update({
-        id: currentDevice.id as string,
-        c8y_IsBookmarked: this.isBookmarked ? null : {},
-      });
-      this.isBookmarked = !this.isBookmarked;
-    }
-    this.navigator.refresh();
   }
 }
